21d01031d5f66cc63beda8b4f46f6824f9b22148
Konstantinos Papadopoulos <k.papadopoulos@sgdigital.com>
2021-02-01 17:27:38 +0200

    Initial commit

---
+++ src/main/java/myflink/SocketWindowWordCount.java
@@ -0,0 +1,41 @@
+package myflink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+import java.util.Arrays;
+
+public class SocketWindowWordCount {
+
+    public static void main(String[] args) throws Exception {
+        // Create a StreamExecutionEnvironment
+        // This is an entry class that can be used to set parameters, create data sources, and submit tasks.
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Tuple2<String, Integer>> dataStream = env
+                .socketTextStream("localhost", 9999)
+                .flatMap(new Splitter())
+                .keyBy(value -> value.f0)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+                .sum(1);
+
+        dataStream.print();
+
+        env.execute("Socket Window WordCount");
+    }
+
+    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) {
+            Arrays
+                    .stream(sentence.split(" "))
+                    .forEach(word -> out.collect(new Tuple2<>(word, 1)));
+        }
+    }
+
+}
